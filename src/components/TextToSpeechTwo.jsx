0413156b9c69300784ff6b7a5f7cf7445a387bed
sm-alamin <sma.fkml@gmail.com>
2023-05-30 13:31:40 +0600

    add speed selection basic design

--- src/components/TextToSpeechTwo.jsx
+++ src/components/TextToSpeechTwo.jsx
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 import { MdRecordVoiceOver } from "react-icons/md";
 import { FiSkipBack, FiSkipForward } from "react-icons/fi";
-import { BsPlayCircle,BsFillPauseCircleFill,BsChevronCompactDown } from "react-icons/bs";
+import {
+  BsPlayCircle,
+  BsFillPauseCircleFill,
+  BsChevronCompactDown,
+} from "react-icons/bs";
 import { TiTickOutline } from "react-icons/ti";
 import img1 from "../assets/english.png";
 
 const TextToSpeechTwo = () => {
   const [showOption, setShowOption] = useState(false);
+  const [showSpeed, setShowSpeed] = useState(false);
   const [playIcon, setPlayIcon] = useState(true);
-
+  const [speedValue, setspeedValue] = useState(250);
+  const handleRatingChange = (event) => {
+    const value = parseFloat(event.target.value);
+    setspeedValue(value);
+  };
   const handleVoiceIconClick = () => {
     setShowOption(!showOption);
   };
-
+  const handleSpeedIconClick = () => {
+    setShowSpeed(!showSpeed);
+  };
+  let backgroundClass = "";
+  if (speedValue >= 300 && speedValue < 600) {
+    backgroundClass = "fast-background";
+  } else if (speedValue >= 600 && speedValue <= 900) {
+    backgroundClass = "very-fast-background";
+  } else if (speedValue >= 200 && speedValue <= 300) {
+    backgroundClass = "normal-background";
+  }
+   else if (speedValue < 200) {
+    backgroundClass = "slow-background";
+  }
   return (
     <div className="position-relative">
       {/* Option */}
@@ -34,10 +56,7 @@ const TextToSpeechTwo = () => {
             <div className="d-flex justify-content-between align-items-center custom-content border-bottom">
               <div>
                 <div>
-                  <img
-                    src={img1} 
-                    alt="english"
-                  />
+                  <img src={img1} alt="english" />
                 </div>
               </div>
               <div>
@@ -52,10 +71,7 @@ const TextToSpeechTwo = () => {
             <div className="d-flex justify-content-between align-items-center  custom-content border-bottom">
               <div>
                 <div>
-                  <img
-                    src={img1}
-                    alt="english"
-                  />
+                  <img src={img1} alt="english" />
                 </div>
               </div>
               <div>
@@ -66,14 +82,11 @@ const TextToSpeechTwo = () => {
                 <TiTickOutline />
               </div>
             </div>
-            
+
             <div className="d-flex justify-content-between align-items-center custom-content border-bottom">
               <div>
                 <div>
-                  <img
-                    src={img1}
-                    alt="english"
-                  />
+                  <img src={img1} alt="english" />
                 </div>
               </div>
               <div>
@@ -84,14 +97,11 @@ const TextToSpeechTwo = () => {
                 <TiTickOutline />
               </div>
             </div>
-            
+
             <div className="d-flex justify-content-between align-items-center  custom-content border-bottom">
               <div>
                 <div>
-                  <img
-                    src={img1} 
-                    alt="english"
-                  />
+                  <img src={img1} alt="english" />
                 </div>
               </div>
               <div>
@@ -102,14 +112,11 @@ const TextToSpeechTwo = () => {
                 <TiTickOutline />
               </div>
             </div>
-            
+
             <div className="d-flex justify-content-between align-items-center custom-content border-bottom">
               <div>
                 <div>
-                  <img
-                    src={img1} 
-                    alt="english"
-                  />
+                  <img src={img1} alt="english" />
                 </div>
               </div>
               <div>
@@ -119,6 +126,59 @@ const TextToSpeechTwo = () => {
               <div>
                 <TiTickOutline />
               </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {showSpeed && (
+        <div className="text-black">
+          <div className="adjusted-position1 bg-white p-2">
+            <div className="d-flex gap-3 ">
+              <h3 className="fs-5">Pick Your Favourite Voice </h3>
+              <span className="fs-5">
+                <BsChevronCompactDown />
+              </span>
+            </div>
+
+            <p className="custom-font-size">
+              Improve comprehension with AI voices
+            </p>
+          </div>
+          <div className="adjusted-position bg-white">
+            <div className=" mt-5 d-flex">
+              <label className="text-gray-600 position-absolute top-0">
+                Selected Speed: {speedValue}
+              </label>
+              <input
+                type="range"
+                id="rating"
+                name="rating"
+                placeholder="Write Rating of Toy"
+                min="100"
+                max="900"
+                step="10"
+                defaultValue={560}
+                value={speedValue}
+                onChange={handleRatingChange}
+                required
+                className="verticle-align mt-1 block  field-input  w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              />
+
+<div>
+            <div className={backgroundClass === "normal-background" ? "normal-background" : ""}>
+              Normal
+            </div>
+            <div className={backgroundClass === "fast-background" ? "fast-background" : ""}>
+              Fast
+            </div>
+            <div className={backgroundClass === "very-fast-background" ? "very-fast-background" : ""}>
+              Very Fast
+            </div>
+            <div className={backgroundClass === "slow-background" ? "slow-background" : ""}>
+              Slow
+            </div>
+          </div>
+
             </div>
           </div>
         </div>
@@ -163,21 +223,37 @@ const TextToSpeechTwo = () => {
           </div>
           {/* Controls */}
           <div className="d-flex gap-3">
-           <div> <FiSkipBack /></div>
-           {
-            playIcon ?
-              <div><BsPlayCircle onClick={()=> setPlayIcon(!playIcon)} className="fs-3"style={{color: "#6610f2"}}/></div>
-              :
-             
-              <div><BsFillPauseCircleFill onClick={()=> setPlayIcon(!playIcon)} className="fs-3"style={{color: "#6610f2"}}/></div>
-             
-           }
-            <div><FiSkipForward /></div>
+            <div>
+              {" "}
+              <FiSkipBack />
+            </div>
+            {playIcon ? (
+              <div>
+                <BsPlayCircle
+                  onClick={() => setPlayIcon(!playIcon)}
+                  className="fs-3"
+                  style={{ color: "#6610f2" }}
+                />
+              </div>
+            ) : (
+              <div>
+                <BsFillPauseCircleFill
+                  onClick={() => setPlayIcon(!playIcon)}
+                  className="fs-3"
+                  style={{ color: "#6610f2" }}
+                />
+              </div>
+            )}
+            <div>
+              <FiSkipForward />
+            </div>
           </div>
           {/* Speed */}
           <div>
             <div className="">
-              <p className="cursor-pointer">1.00x</p>
+              <p className="cursor-pointer" onClick={handleSpeedIconClick}>
+                1.00x
+              </p>
             </div>
           </div>
         </div>
